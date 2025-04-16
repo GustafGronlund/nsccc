e3a0d3e20f9fdefa68758f87f6695bde53deb131
GustafGronlund <gustafgronlund@gmail.com>
2025-04-16 09:59:17 +0200

    feat: add pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  HomePage,
+  GalleryPage,
+  AboutPage,
+  RegistrationPage,
+  ContactPage,
+} from "./pages";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
