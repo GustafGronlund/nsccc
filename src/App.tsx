2b82e301c48b8dc0d7f7dd953cc3fbe9ab85a128
GustafGronlund <gustafgronlund@gmail.com>
2025-04-22 09:53:39 +0200

    feat: add smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
   HomePage,
   GalleryPage,
   AboutPage,
   RegistrationPage,
   ContactPage,
-} from "./pages";
-import { Header } from "./components";
-import "./index.css";
+} from './pages';
+import { Header } from './components';
+import './index.css';
+import Lenis from 'lenis';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
