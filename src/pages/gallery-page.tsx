139add9f8cb741816df619ecfb641cd81aa854bc
GustafGronlund <gustafgronlund@gmail.com>
2025-04-25 09:25:48 +0200

    feat: add year and overlay to gallery images

--- src/pages/gallery-page.tsx
+++ src/pages/gallery-page.tsx
@@ -6,25 +6,30 @@ export const GalleryPage = () => {
   const ScrollReveal = useScrollReveal;
 
   return (
-    <div className="max-w-10xl mx-auto mt-30 px-8">
+    <div className="mx-auto mt-30 w-full px-8">
       <ScrollReveal delay={0} initialY={10} duration={1}>
         <div className="grid grid-cols-2 gap-10 md:grid-cols-4">
-          {galleryPageImages.map((item, index) => (
+          {galleryPageImages.map((galleryImage, index) => (
             <Link
-              to={item.path}
+              to={galleryImage.path}
               key={index}
               target="_blank"
               rel="noopener noreferrer"
             >
               <div className="flex flex-col">
-                <div className="mb-2 aspect-square overflow-hidden">
+                <div className="group relative mb-2 aspect-square overflow-hidden">
                   <img
-                    src={item.image}
-                    alt={`Gallery image ${item.year}`}
-                    className="h-full w-full cursor-pointer object-cover transition-transform duration-[1500ms] hover:scale-103"
+                    src={galleryImage.image}
+                    alt={`Image from the event year ${galleryImage.year}`}
+                    className="h-full w-full cursor-pointer object-cover transition-transform duration-[1500ms] group-hover:scale-103"
                   />
+                  <div className="absolute inset-0 bg-[#29ABE2] opacity-70 transition-opacity duration-500 ease-in-out group-hover:opacity-0"></div>
+                  <div className="pointer-events-none absolute inset-0 flex items-center justify-center transition-opacity duration-500 ease-in-out group-hover:opacity-0">
+                    <span className="text-4xl font-medium text-white lg:text-6xl">
+                      {galleryImage.year}
+                    </span>
+                  </div>
                 </div>
-                <div className="font-normal text-[#383838]">{item.year}</div>
               </div>
             </Link>
           ))}
