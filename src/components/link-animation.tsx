4235a4fa87a0469f4aad0f6f0f9dd26d0840e437
GustafGronlund <gustafgronlund@gmail.com>
2025-09-30 10:51:10 +0200

    feat: add new menu item

--- src/components/link-animation.tsx
+++ src/components/link-animation.tsx
@@ -15,6 +15,8 @@ export const LinkAnimation = ({
 }: LinkAnimationProps) => {
   const [isHover, setIsHover] = useState<boolean>(false);
 
+  const isExternalLink = href.startsWith('http') || href.includes('.pdf');
+
   return (
     <motion.li
       onMouseEnter={() => {
@@ -25,24 +27,45 @@ export const LinkAnimation = ({
       }}
       className="relative flex h-fit items-center justify-center overflow-y-hidden"
     >
-      <Link to={href}>
-        <motion.p
-          initial={{ translateY: 0 }}
-          animate={{ translateY: isHover ? 20 : 0 }}
-          transition={{ duration: 0.5 }}
-          className={`absolute cursor-pointer font-sans text-lg leading-tight tracking-tighter ${activePath === href ? 'text-[#29ABE2]' : 'text-[#383838]'}`}
-        >
-          {title}
-        </motion.p>
-        <motion.p
-          initial={{ translateY: -30 }}
-          animate={{ opacity: 1, translateY: isHover ? 0 : -30 }}
-          transition={{ duration: 0.5 }}
-          className={`cursor-pointer font-sans text-lg leading-tight tracking-tighter ${activePath === href ? 'text-[#29ABE2]' : 'text-[#383838]'}`}
-        >
-          {title}
-        </motion.p>
-      </Link>
+      {isExternalLink ? (
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          <motion.p
+            initial={{ translateY: 0 }}
+            animate={{ translateY: isHover ? 20 : 0 }}
+            transition={{ duration: 0.5 }}
+            className={`absolute cursor-pointer font-sans text-lg leading-tight tracking-tighter ${activePath === href ? 'text-[#29ABE2]' : 'text-[#383838]'}`}
+          >
+            {title}
+          </motion.p>
+          <motion.p
+            initial={{ translateY: -30 }}
+            animate={{ opacity: 1, translateY: isHover ? 0 : -30 }}
+            transition={{ duration: 0.5 }}
+            className={`cursor-pointer font-sans text-lg leading-tight tracking-tighter ${activePath === href ? 'text-[#29ABE2]' : 'text-[#383838]'}`}
+          >
+            {title}
+          </motion.p>
+        </a>
+      ) : (
+        <Link to={href}>
+          <motion.p
+            initial={{ translateY: 0 }}
+            animate={{ translateY: isHover ? 20 : 0 }}
+            transition={{ duration: 0.5 }}
+            className={`absolute cursor-pointer font-sans text-lg leading-tight tracking-tighter ${activePath === href ? 'text-[#29ABE2]' : 'text-[#383838]'}`}
+          >
+            {title}
+          </motion.p>
+          <motion.p
+            initial={{ translateY: -30 }}
+            animate={{ opacity: 1, translateY: isHover ? 0 : -30 }}
+            transition={{ duration: 0.5 }}
+            className={`cursor-pointer font-sans text-lg leading-tight tracking-tighter ${activePath === href ? 'text-[#29ABE2]' : 'text-[#383838]'}`}
+          >
+            {title}
+          </motion.p>
+        </Link>
+      )}
     </motion.li>
   );
 };
